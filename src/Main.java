795908c7ab33a1bb322f12e75e4f3ef810222d8e
GitIvona <ivona.85@mail.ru>
2022-05-24 17:04:21 +0300

    Reformat code

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main {
         }
 
 
-        System.out.println("Бонус равен : " +  bonusRubles + " руб.");
+        System.out.println("Бонус равен : " + bonusRubles + " руб.");
         System.out.println("Итоговый счет равен : " + sum + " руб.");
 
 
