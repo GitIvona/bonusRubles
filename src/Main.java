7d4e68d2e1a5db110c20197acfe975074c38aa0a
GitIvona <ivona.85@mail.ru>
2022-05-24 21:02:09 +0300

    add totSum

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,20 @@
 public class Main {
     public static void main(String[] args) {
         int currentAccount = 10;
-        int cashInflow = 1001;
+        int cashInflow = 1005;
         int bonusRubles;
-        int sum = currentAccount + cashInflow;
+        //int sum = currentAccount + cashInflow;
+
         if (cashInflow > 1000) {
             bonusRubles = cashInflow / 100;
         } else {
             bonusRubles = 0;
-        }
 
+        }
+        int totSum = currentAccount + cashInflow + bonusRubles;
 
         System.out.println("Бонус равен : " + bonusRubles + " руб.");
-        System.out.println("Итоговый счет равен : " + sum + " руб.");
-
+        System.out.println("Итоговый счет равен : " + totSum + " руб.");
 
     }
 }
