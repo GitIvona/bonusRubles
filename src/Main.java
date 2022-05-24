85660bdf808a3c16f7709f5bc3ce6b4c86cf4ad6
GitIvona <ivona.85@mail.ru>
2022-05-24 15:59:40 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        int currentAccount = 10;
+        int cashInflow = 1001;
+        int bonusRubles;
+        int sum = currentAccount + cashInflow;
+        if (cashInflow > 1000) {
+            bonusRubles = cashInflow / 100;
+        } else {
+            bonusRubles = 0;
+        }
+
+
+        System.out.println("Бонус равен : " +  bonusRubles + " руб.");
+        System.out.println("Итоговый счет равен : " + sum + " руб.");
+
+
+    }
+}
